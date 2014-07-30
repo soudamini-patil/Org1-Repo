d0836d0412349ddfe91a82f078d9cd901be61d77
soudamini-patil <soudamini.patil@cumminscollege.in>
2014-07-30 15:25:21 +0530

    Update welcome.cpp

--- welcome.cpp
+++ welcome.cpp
@@ -6,3 +6,11 @@ void main()
 }
 
 // This is a blank comment for versioning purpose
+
+// This method is added after the Org1-Repo is forked from Org1/Org1-Repo
+
+double select_number ()
+{
+   // This is a  random generator
+   return random();
+}
